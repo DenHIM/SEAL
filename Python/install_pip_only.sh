a1f0c433b570f66cf99c5d956b4c8f35b73db25c
Denis <denis.guryev@hotmail.com>
2021-01-10 00:42:21 -0500

    Updated install for Ubuntu, Python 3.6; added pip-only install

--- Python/install_pip_only.sh
+++ Python/install_pip_only.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cd ../../
-git clone https://github.com/DenHIM/pytorch_DGCNN
-cd pytorch_DGCNN
-cd lib
-make -j4
-cd "$(dirname "$0")"
 pip3 install --user numpy
 pip3 install --user scipy
 pip3 install --user networkx
