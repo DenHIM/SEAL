ca3778ac3660fb18aa649cef1113c95d3cb0906f
Denis <denis.guryev@hotmail.com>
2021-01-10 00:45:32 -0500

    Added cython due to an sklearn error

--- Python/install.sh
+++ Python/install.sh
@@ -10,5 +10,6 @@ pip3 install --user numpy
 pip3 install --user scipy
 pip3 install --user networkx
 pip3 install --user tqdm
+pip3 install --user cython
 pip3 install --user sklearn
 pip3 install --user gensim
